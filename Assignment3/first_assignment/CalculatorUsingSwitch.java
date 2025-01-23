6d57fbb6ae0d19b95b657ee9c4e93de688b12dc0
Kunnaaallllll <869796kunalchaudhary@gmail.com>
2025-01-23 19:29:36 +0530

    Assignment 3 committed

---
+++ Assignment3/first_assignment/CalculatorUsingSwitch.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class CalculatorUsingSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double first = sc.nextDouble();
+        double second = sc.nextDouble();
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println(first + second);
+
+                break;
+            case '-':
+                System.out.println(first - second);
+
+                break;
+            case '*':
+                System.out.println(first * second);
+
+                break;
+            case '/':
+                System.out.println(first / second);
+
+                break;
+            default:
+                System.out.println("Invalid operation");
+                break;
+        }
+
+    }
+}
